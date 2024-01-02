bc18e23eca51bbe7c5ca2974599efcb28ac01f85
Aamsher <hpandian008@gmail.com>
2024-01-02 18:36:57 +0530

    middleware update

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,18 +1,13 @@
 module.exports = ({ env }) => [
   {
-    settings: {
-      cors: {
-        enabled: true,
-        origin: env("CORS_ORIGIN", "https://backend-wpof.onrender.com/"),
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        headers: ["Content-Type", "Authorization"],
-      },
+    name: "strapi::cors",
+    config: {
+      origin: ["https://backend-wpof.onrender.com/"],
     },
   },
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
