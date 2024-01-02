1ee15e2e95e7b6722b5885ef7ad00c2be149313c
Aamsher <hpandian008@gmail.com>
2024-01-02 18:16:43 +0530

    updated middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ["https://your-frontend-app.com"], // Replace with your frontend app URL
+      },
+    },
+  },
+
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
